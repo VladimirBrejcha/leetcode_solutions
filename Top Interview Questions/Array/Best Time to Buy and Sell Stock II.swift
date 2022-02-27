d8b4ccedff3c58078b8ff27d90c698555a5b8e6a
Vladimir Korolev <vladimirbrejcha@icloud.com>
2022-02-27 09:46:20 +0300

    Create Best Time to Buy and Sell Stock II.swift

---
+++ Top Interview Questions/Array/Best Time to Buy and Sell Stock II.swift
@@ -0,0 +1,40 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
+
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        
+        var boughtPrice: Int?
+        var profit: Int = 0
+        
+        for i in 0..<prices.count {
+            
+            let current = prices[i]
+            
+            if let price = boughtPrice {
+             
+                // corner case - end of the array
+                if i == prices.count - 1 {
+                    profit += current - price
+                    break
+                }
+                
+                if price < current {
+                    
+                    // corner case, next price is even better
+                    if current < prices[i + 1] {
+                        continue
+                    }
+                    
+                    profit += current - price
+                    boughtPrice = nil
+                }
+            } else if i < prices.count - 1 {
+                if current < prices[i + 1] {
+                    boughtPrice = current
+                }
+            }
+        }
+        
+        return profit
+    }
+}
