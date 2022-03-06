d8fe7e639b3443642718af18a767074557ff323c
Vladimir Korolev <vladimirbrejcha@icloud.com>
2022-03-07 02:22:56 +0300

    Update Rotate Array.swift

--- Top Interview Questions/Array/Rotate Array.swift
+++ Top Interview Questions/Array/Rotate Array.swift
@@ -6,3 +6,16 @@ class Solution {
         nums = Array(nums[nums.count - kNormilized..<nums.count] + nums[0..<nums.count - kNormilized])
     }
 }
+
+class Solution {
+    func rotate(_ nums: inout [Int], _ k: Int) {
+        let kNormilized = k % nums.count
+        
+        var queue: [Int] = Array(nums[nums.count - kNormilized..<nums.count])
+        
+        for i in 0..<nums.count {
+            queue.append(nums[i])
+            nums[i] = queue[i]
+        }
+    }
+}
