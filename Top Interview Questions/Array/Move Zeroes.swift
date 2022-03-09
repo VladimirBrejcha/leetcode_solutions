23537c9fe259f53e37a8d97356e8c4c7f1b88b55
Vladimir Korolev <vladimirbrejcha@icloud.com>
2022-03-10 00:34:14 +0300

    Create Move Zeroes.swift

---
+++ Top Interview Questions/Array/Move Zeroes.swift
@@ -0,0 +1,32 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/567/
+
+class Solution {
+    func moveZeroes(_ nums: inout [Int]) {
+        if nums.count == 1 { return }
+        
+        var slow: Int?
+        
+        for fast in 0..<nums.count {
+            if nums[fast] == 0 {
+                continue
+            }
+            
+            if slow == nil {
+                if fast == 0 {
+                    slow = 0
+                    continue
+                }
+                nums[0] = nums[fast]
+                nums[fast] = 0
+                slow = 0
+                continue
+            }
+            
+            if fast - slow! > 0 && nums[slow! + 1] == 0 {
+                nums[slow! + 1] = nums[fast]
+                nums[fast] = 0
+            }
+            slow! += 1
+        }
+    }
+}
