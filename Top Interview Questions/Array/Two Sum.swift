ecd0fbd77b074c62a3510c529a13354360eb073e
Vladimir Korolev <vladimirbrejcha@icloud.com>
2022-03-10 01:35:01 +0300

    Create Two Sum.swift

---
+++ Top Interview Questions/Array/Two Sum.swift
@@ -0,0 +1,20 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/546/
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        if nums.count == 2 { return [0, 1] }
+        
+        var histogram = Dictionary<Int, Int>(minimumCapacity: nums.count)
+        for i in 0..<nums.count {
+            histogram[nums[i] - target] = i
+        }
+        
+        for i in 0..<nums.count {
+            let i2 = histogram[-nums[i]]
+            if i2 == nil || i2 == i { continue }
+            return [i, i2!]
+        }
+        
+        return []
+    }
+}
