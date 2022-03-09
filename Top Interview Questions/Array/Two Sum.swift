c91e152c08fb7c368124dcaca9ba28b44340c6dd
Vladimir Korolev <vladimirbrejcha@icloud.com>
2022-03-10 01:37:22 +0300

    Update Two Sum.swift

--- Top Interview Questions/Array/Two Sum.swift
+++ Top Interview Questions/Array/Two Sum.swift
@@ -18,3 +18,19 @@ class Solution {
         return []
     }
 }
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        if nums.count == 2 { return [0, 1] }
+        
+        var histogram = Dictionary<Int, Int>(minimumCapacity: nums.count)
+        for i in 0..<nums.count {
+            if let i2 = histogram[-nums[i]] {
+                return [i, i2]
+            }
+            histogram[nums[i] - target] = i
+        }
+        
+        return []
+    }
+}
