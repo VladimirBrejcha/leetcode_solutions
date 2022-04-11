78d567edf2fd1ccd85125d5b67f0a6b72f3a18f5
Vladimir Korolev <vladimirbrejcha@icloud.com>
2022-04-11 15:25:38 +0300

    Update Stack.swift

--- Stack.swift
+++ Stack.swift
@@ -68,20 +68,70 @@ extension Stack: Hashable where Element: Hashable { }
 extension Stack: Decodable where Element: Decodable { }
 extension Stack: Encodable where Element: Encodable { }
 
+// MARK: - Tests -
+
+// MARK: - Accessors Tests
+
+print("=== Accessors Tests ===")
+
+print("Create [0, 1, 2] stack")
 var stackA: Stack<Int> = [0, 1, 2]
 print(stackA)
+print("pop")
 stackA.pop()
 print(stackA)
+print("push 17")
 stackA.push(17)
 print(stackA)
 
-for element in stackA.reversed() {
+// MARK: - Sequence Tests
+
+print("=== Sequence Tests ===")
+
+print("All elements of [0, 1, 17] stack")
+for element in stackA {
    print(element)
 }
 
-let stackB: Stack<Int> = [0, 1, 17]
-print(stackA == stackB)
+// MARK: - Equatable & Hashable Tests
 
+print("=== Equatable & Hashable Tests ===")
+
+print("Create another [0, 1, 17] stack")
+let stackB: Stack<Int> = [0, 1, 17]
+print("Equality:")
+print(stackA == stackB)
+print("Equality of hashes:")
+print(stackA.hashValue == stackB.hashValue)
+
+// MARK: - Debug logging tests
+
+print("=== Debug logging tests ===")
+
+print("dump [1, 2] stack:")
 let arr1 = [1,2]
 dump(arr1)
+print("dump [0, 1, 17] stack:")
 dump(stackB)
+
+// MARK: - Copy On Write Tests
+
+print("=== Copy On Write Tests ===")
+
+let stack1: Stack<Int> = [1, 2]
+var stack2 = stack1
+
+print("stack1: \(stack1)")
+print("stack2: \(stack2)")
+
+print("push 3 to stack2")
+stack2.push(3)
+print("stack1: \(stack1)")
+print("stack2: \(stack2)")
+
+print("pop from stack2")
+stack2.pop()
+print("pop from stack2")
+stack2.pop()
+print("stack1: \(stack1)")
+print("stack2: \(stack2)")
